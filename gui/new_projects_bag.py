03237aac76f4a1b5ac7ca56836620964b5223777
Nobile <m.s.nobile@tue.nl>
2019-10-11 13:45:10 +0200

    Removed old directory structure

--- gui/new_projects_bag.py
+++
@@ -1,9 +0,0 @@
-class Projects(object):
-
-	def __init__(self):
-		self._projects = []
-
-	def add(self, p):
-		if (p not in self._projects) and (p != ""):
-			self._projects.append(p)
-		return self._projects
